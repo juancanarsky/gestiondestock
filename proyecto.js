0d5cbd7c5ba746ad9652a0dcb8634fa4b65ab78e
Juan Ignacio Canarsky <juancanarsky@gmail.com>
2023-11-30 16:18:02 -0300

    Segunda preentrega

--- proyecto.js
+++ proyecto.js
@@ -2,6 +2,10 @@ function opcionIncorrecta(){
     alert("Opción incorrecta");
 }
 
+function volverAlMenu(){
+    alert('Volviendo al menú...');
+}
+
 function saludoBienvenida(nombre){
     alert(`Bienvenido/a, ${nombre}`)
 }
@@ -14,22 +18,22 @@ function recorrerLista(){
     productos.forEach((articulo, index) => {console.log(articulo.producto, index)})
 }
 
-function seguirAgregando(productoProducto, categoriaProducto, stockProducto, precioProducto){
+function seguirAgregando(productoProducto, categoriaProducto, stockProducto, precioProducto) {
     productoProducto = prompt("Ingrese el nombre del producto a agregar:");
-    while (!productoProducto || !productoProducto.trim()) {
+    while (!productoProducto || productoProducto === false) {
         productoProducto = prompt("No se permiten campos vacíos, por favor ingrese producto a agregar:");
+    }
+    categoriaProducto = prompt("Ingrese la categoría del producto a agregar: ");
+    while (!categoriaProducto || categoriaProducto === false) {
+        categoriaProducto = prompt("No se permiten campos vacíos, por favor ingrese la categoría del producto a agregar:");
+    }
+    stockProducto = +prompt ("Ingrese el stock del producto a agregar: ");
+    while (!stockProducto) {
+        stockProducto = +prompt("Solo se permiten números, por favor ingrese el stock del producto a agregar:");
         }
-        categoriaProducto = prompt ("Ingrese la categoria del producto a agregar: ");
-        while (!categoriaProducto || !categoriaProducto.trim()) {
-            categoriaProducto = prompt("No se permiten campos vacíos, por favor ingrese la categoría del producto a agregar:");
-        }
-        stockProducto = +prompt ("Ingrese el stock del producto a agregar: ");
-        while (!stockProducto) {
-            stockProducto = +prompt("Solo se permiten números, por favor ingrese el stock del producto a agregar:");
-        }
-        precioProducto = +prompt ("Ingrese el precio del producto a agregar: ");
-        while (!precioProducto) {
-            precioProducto = +prompt("Solo se permiten números, por favor ingrese el precio del producto a agregar:");
+    precioProducto = +prompt ("Ingrese el precio del producto a agregar: ");
+    while (!precioProducto) {
+        precioProducto = +prompt("Solo se permiten números, por favor ingrese el precio del producto a agregar:");
     }
     let nuevoProducto = new Producto(productoProducto, categoriaProducto, stockProducto, precioProducto);
     return nuevoProducto
@@ -90,12 +94,18 @@ while (opcion != 6){
             let nuevoProducto = seguirAgregando(productoProducto, categoriaProducto, stockProducto, precioProducto);
             productos.push(new Producto(nuevoProducto.producto, nuevoProducto.categoria, nuevoProducto.stock, nuevoProducto.precio));
             console.log(nuevoProducto);
-            decision = +prompt('¿Quiere agregar otro producto?' + '\n' + '"1" - Sí' + '\n' + '"2" - No');
-            if (decision !== 1 && decision !== 2) {
-                opcionIncorrecta();
-            }}
-            if (decision === 2) {
-                alert('Volviendo al menú...');
+            let entradaValida = false;
+            while (!entradaValida) {
+                decision = +prompt('¿Quiere agregar otro producto?' + '\n' + '"1" - Sí' + '\n' + '"2" - No');
+                if (decision !== 1 && decision !== 2) {
+                    opcionIncorrecta();
+                } else {
+                    entradaValida = true;
+                }
+            }
+        }
+        if (decision === 2) {
+            volverAlMenu();
         }
     }
 
@@ -113,7 +123,7 @@ while (opcion != 6){
                     alert ('Te dije que no se puede eliminar todaviaaa!!');
             }
             else if (decision == 2){
-                alert ('Volviendo al menú...');
+                volverAlMenu();
                 }
             else {
                 opcionIncorrecta();
@@ -135,7 +145,7 @@ while (opcion != 6){
                 alert ('Te dije que no se puede modificar todaviaaa!!');
             }
             else if (decision == 2){
-                alert ('Volviendo al menú...');
+                volverAlMenu();
                 }
             else {
                 opcionIncorrecta();
@@ -153,7 +163,7 @@ while (opcion != 6){
                 opcionIncorrecta();
             }
         } while (decision !== 2);
-        alert('Volviendo al menú...');
+        volverAlMenu();
     }
 
     if (opcion == 6){
