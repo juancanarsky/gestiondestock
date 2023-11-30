98becb3ecbb15b3ef6e470fc748569d16d19ca94
Juan Ignacio Canarsky <juancanarsky@gmail.com>
2023-11-29 21:00:43 -0300

    Segunda preentrega

--- proyecto.js
+++ proyecto.js
@@ -9,27 +9,30 @@ function saludoBienvenida(nombre){
 function saludoDespedida(nombre){
     alert(`Nos vemos pronto, ${nombre}`)
 }
-// Necesito que solo me acepte números
-function seguirAgregando(){
+
+function recorrerLista(){
+    productos.forEach((articulo, index) => {console.log(articulo.producto, index)})
+}
+
+function seguirAgregando(productoProducto, categoriaProducto, stockProducto, precioProducto){
     productoProducto = prompt("Ingrese el nombre del producto a agregar:");
     while (!productoProducto || !productoProducto.trim()) {
         productoProducto = prompt("No se permiten campos vacíos, por favor ingrese producto a agregar:");
+        }
+        categoriaProducto = prompt ("Ingrese la categoria del producto a agregar: ");
+        while (!categoriaProducto || !categoriaProducto.trim()) {
+            categoriaProducto = prompt("No se permiten campos vacíos, por favor ingrese la categoría del producto a agregar:");
+        }
+        stockProducto = +prompt ("Ingrese el stock del producto a agregar: ");
+        while (!stockProducto) {
+            stockProducto = +prompt("Solo se permiten números, por favor ingrese el stock del producto a agregar:");
+        }
+        precioProducto = +prompt ("Ingrese el precio del producto a agregar: ");
+        while (!precioProducto) {
+            precioProducto = +prompt("Solo se permiten números, por favor ingrese el precio del producto a agregar:");
     }
-    categoriaProducto = prompt ("Ingrese la categoria del producto a agregar: ");
-    while (!categoriaProducto || !categoriaProducto.trim()) {
-        categoriaProducto = prompt("No se permiten campos vacíos, por favor ingrese la categoría del producto a agregar:");
-    }
-    stockProducto = +prompt ("Ingrese el stock del producto a agregar: ");
-    while (!stockProducto || !stockProducto.trim()) {
-        stockProducto = +prompt("Solo se permiten números, por favor ingrese el stock del producto a agregar:");
-    }
-    precioProducto = +prompt ("Ingrese el precio del producto a agregar: ");
-    while (!precioProducto || !precioProducto.trim()) {
-        precioProducto = +prompt("Solo se permiten números, por favor ingrese el precio del producto a agregar:");
-    }
-    nuevoProducto = new Producto(productoProducto, categoriaProducto, stockProducto, precioProducto);
-    productos.push(nuevoProducto);
-    alert ('Producto agregado correctamente.');
+    let nuevoProducto = new Producto(productoProducto, categoriaProducto, stockProducto, precioProducto);
+    return nuevoProducto
 }
 
 function buscarProducto(productos, busqueda) {
@@ -77,41 +80,22 @@ while (opcion != 6){
     + '"6" - Salir');
 
     if (opcion == 1){
-        productos.forEach((articulo, index) => {console.log(articulo.producto, index)})
+        recorrerLista()
     }
 
-    if (opcion == 2){
-        let productoProducto = prompt("Ingrese el nombre del producto a agregar:");
-        while (!productoProducto || !productoProducto.trim()) {
-            productoProducto = prompt("No se permiten campos vacíos, por favor ingrese producto a agregar:");
-        }
-        let categoriaProducto = prompt ("Ingrese la categoria del producto a agregar: ");
-        while (!categoriaProducto || !categoriaProducto.trim()) {
-            categoriaProducto = prompt("No se permiten campos vacíos, por favor ingrese producto a agregar:");
-        }
-            let stockProducto = +prompt ("Ingrese el stock del producto a agregar: ");
-        while (!stockProducto || !stockProducto.trim()) {
-            stockProducto = prompt("No se permiten campos vacíos, por favor ingrese producto a agregar:");
-        }
-            let precioProducto = +prompt ("Ingrese el precio del producto a agregar: ");
-        while (!precioProducto || !precioProducto.trim()) {
-            precioProducto = prompt("No se permiten campos vacíos, por favor ingrese producto a agregar:");
-        }
-            let nuevoProducto = new Producto(productoProducto, categoriaProducto, stockProducto, precioProducto);
-        productos.push(nuevoProducto);
-        alert ('Producto agregado correctamente.');
-        let decision = 0
-        while(decision != 1 && decision != 2){
-            decision = +prompt ('Quiere agregar otro producto?' + '\n' + '"1" - Sí' + '\n' + '"2" - No');
-            if (decision == 1){
-                seguirAgregando();
-            }
-            else if (decision ==2){
-                alert ('Volviendo al menú...');
-            }
-            else {
+    if (opcion == 2) {
+        let decision = 1;
+        while (decision === 1) {
+            let productoProducto, categoriaProducto, stockProducto, precioProducto;
+            let nuevoProducto = seguirAgregando(productoProducto, categoriaProducto, stockProducto, precioProducto);
+            productos.push(new Producto(nuevoProducto.producto, nuevoProducto.categoria, nuevoProducto.stock, nuevoProducto.precio));
+            console.log(nuevoProducto);
+            decision = +prompt('¿Quiere agregar otro producto?' + '\n' + '"1" - Sí' + '\n' + '"2" - No');
+            if (decision !== 1 && decision !== 2) {
                 opcionIncorrecta();
-                }
+            }}
+            if (decision === 2) {
+                alert('Volviendo al menú...');
         }
     }
 
